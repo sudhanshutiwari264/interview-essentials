d4c111979a96311f007a0b92844c0a19d55b2076
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-02-16 19:04:32 +0000

    convsersion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "sudha"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN 
+//True => 1; fasle => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "sudha" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
