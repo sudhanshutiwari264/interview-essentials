037b462e3a5dda577bb60c6944f0ace936371072
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-02-16 18:42:29 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log("sudhanshu");
+
+console.log(3
+    +
+    3); // code readabily should be high
+
+// preferred 
+console.log(3+3)
+
+//ECMA STANDARDS FOLLOW krte hain
+// MDN ka bhi acha hai documentation
+
+let name = "Sudhansh"
+let age = 21
+let isLoggedIn = false 
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null : standalone value hai 
+// undefined =>
+// symbol > unique  > react and all mai use hota hai 
+// premetives data  types hain uper wale 
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
