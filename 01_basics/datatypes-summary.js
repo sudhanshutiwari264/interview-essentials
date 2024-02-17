7ba36e3e0e0fa6cf1422ce90bb7f3a70eb3399fd
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-02-17 13:38:32 +0000

    dataTypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive 
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id === anotherId);
+
+// const bigNumber = 342545658415213345686n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions 
+
+const heros = ["shaktiman","naagraj","doga"] ;
+let myObj = {
+    name : "Suhdanshu",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof heros);
