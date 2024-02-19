ee80d08ccf0793dd70964e88dd552357a8af798f
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-02-19 14:19:55 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,42 @@
+// array
+
+const myArray = [0,1,2,3,4,5]
+const myHeros = ["shaktiman" , "naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+
+// console.log(myArray[1]);
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(8)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+
+const newArr = myArray.join()
+
+// console.log(myArray);
+// console.log(typeof newArr);
+
+// slice, splice 
+
+console.log("A",myArray);
+
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArray);
+
+const myn2 = myArray.splice(1,3)
+console.log("C",myArray);
+console.log(myn2);
+
+
+
+
+
+
