998df623311637554e920e6616ba7c81d904e2f4
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-02-20 07:49:57 +0000

    objects basic  2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,52 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname:"Sudhanshu",
+            lastname:"tiwari"
+        }
+    }   
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3: "a", 4:"b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({},obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email: "sudha@gmail.com"
+    },
+    {
+        id: 2,
+        email: "sudha@gmail.com"
+    },  
+    {
+        id:3 ,
+        email: "sudha@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+console.log(tinderUser.hasOwnProperty("isLogged"));
