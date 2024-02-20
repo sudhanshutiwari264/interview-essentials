a937ed7abe52a37c87e22b073de4a06725d0badf
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-02-20 09:23:19 +0000

    scopes basics

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+// var c = 300 
+let a = 300
+if(true){
+    let a = 2
+    const b = 20
+    c = 30
+    console.log("INNER: ",a);
+
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
