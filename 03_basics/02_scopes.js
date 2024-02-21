e1cfa11078f056682d406a7b73b379396a8a38ed
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-02-21 12:40:54 +0000

    expressions and scopes in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,15 +4,62 @@ if(true){
     let a = 2
     const b = 20
     c = 30
-    console.log("INNER: ",a);
+    // console.log("INNER: ",a);
 
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "sudhanshu"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "sudhanshu"
+    if(username === "sudhanshu"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+// +++++++++++++++++ intresting +++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+// expression bolte hai neeche wale ko 
+
+// addtwo(5) yeh wala error hai initial global execution ki wajhe se 
+// hoisiting hai 
+
+const addtwo = function(num){
+    return num + 2
+}
+
+console.log(addtwo(5));
+
