bc77704146884117772c4a481a6e60ce5f4141c4
Sudhanshu tiwari <sudhanshutiwari264@gmail.com>
2024-02-22 14:37:59 +0000

    nullish coalescing operator and terniary operator , scope switch in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+// if
+// const isUserLoggedin = true
+// const temperture = 55
+// if (temperture < 50){
+//     console.log("less than 50");
+// } else{
+//     console.log("temperature is greater then 50");
+// }
+
+// console.log("Execute")
+// <, >, <= ,==, !=, ===, !==
+
+// const score = 200
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`); 
+// }
+
+// console.log(`User power: ${power}`); 
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750");
+// } else if(balance < 950){
+//     console.log("less than 950");
+// } else if(balance < 1200){
+//     console.log("less than 1200");
+// }
+
+const UserLoggedin = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromemail = true
+
+// And && 
+if(UserLoggedin && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
+
+// or ||
+if(loggedInFromGoogle || loggedInFromemail){
+    console.log("User logged in");
+}
+
+
+
+
+
+
